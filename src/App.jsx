1ba565ae62740e571bcdacb26d2fa668fb224cb2
Rohit singh <rohitdev124421@gmail.com>
2025-03-25 16:09:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-500">hello everyone</h1>
+    </>
+  );
+}
+
+export default App;
+ 
